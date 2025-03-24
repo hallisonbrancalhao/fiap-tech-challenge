baac78065e9f1c11ca0f6df2c92e5e597542a464
Halisson Brancalhao <halisson@brancalhao.com.br>
2025-03-24 02:29:23 -0300

    chore: shared deps

--- apps/bytebank/module-federation.config.ts
+++ apps/bytebank/module-federation.config.ts
@@ -3,6 +3,15 @@ import { ModuleFederationConfig } from '@nx/module-federation';
 const config: ModuleFederationConfig = {
   name: 'bytebank',
   remotes: ['dashboard', 'institutional'],
+  shared: (libraryName, defaultConfig) => {
+    if (libraryName.includes(`@apollo/client/`)) {
+      return {
+        ...defaultConfig,
+        version: '3.13.5',
+      }
+    }
+    return defaultConfig;
+  },
 };
 
 export default config;
